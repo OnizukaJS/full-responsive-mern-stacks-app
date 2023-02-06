9944b6bc10d4ea09375ed947bf4d1e3ea8368846
Jean-Baptiste Castillo <jeanbaptiste.castillo@isolutions.ch>
2023-02-06 14:34:56 +0100

    Finished authentication and authorization

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 // CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
@@ -25,15 +27,21 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets"))); // se
 
 // FILE STORAGE
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
   
-  const upload = multer({ storage });
+const upload = multer({ storage });
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/auth", authRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
